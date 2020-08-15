1ac02fbf860f358111ee9cb0164d5514e718e558
Wenping Guo <ybyygu@gmail.com>
2020-08-15 20:22:45 +0800

    添加sp-intel模板, 使用docker来运行intel mpi编译的siesta

---
+++ siesta/sp-intel/submit.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+# require gosh >= 0.0.25
+parent_dir=$(realpath "$BBM_TPL_DIR/..")
+source "$parent_dir/env.rc"
+
+# 1. prepare SIESTA input files
+# require gosh >= 0.0.25
+# docker needs real files
+cp "$parent_dir"/psml/* .
+cp "$BBM_TPL_DIR"/default.fdf .
+
+# 2. read fdf stream from stdin, write stdout to siesta.log for parsing
+tee input.fdf | docker run --rm -i -v $(pwd):$HOME siesta 2>&1 | tee siesta.log > "$BBM_JOB_DIR"/siesta.log
+# siesta cmds in docker can be overridden
+# tee input.fdf | docker run --rm -i -v $(pwd):$HOME siesta bash -l -c "mpirun -np 7 siesta" 2>&1 | tee siesta.log > "$BBM_JOB_DIR"/siesta.log
+
+# 4. parse calculated results
+gosh-adaptor siesta siesta.log
---
+++ siesta/sp/submit.sh
@@ -0,0 +1,1 @@
+/share/apps/siesta/opt/submit.sh
