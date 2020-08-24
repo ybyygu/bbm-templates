32cb3cb92b9ab43a5127776e8876631e2e7512a8
Wenping Guo <ybyygu@gmail.com>
2020-08-24 10:58:18 +0800

    SIESTA: 添加粗算模板

--- siesta/sp-raw/submit.sh
+++ siesta/sp-raw/submit.sh
@@ -8,17 +8,14 @@ parent_dir=/share/apps/siesta
 source "$parent_dir/env.rc"
 
 # 1. prepare SIESTA input files
-cp "$parent_dir"/psml/* . 2>/dev/null
-cp "$BBM_TPL_DIR"/default.fdf . 2>/dev/null
+ln -s "$parent_dir"/psml/* . 2>/dev/null
+ln -s "$BBM_TPL_DIR"/default.fdf . 2>/dev/null
 
 # Make SIESTA resume from last job. This could reduce electronic step costs.
 cp "$BBM_JOB_DIR/siesta.DM" . 2>/dev/null
 
 # 2. read fdf stream from stdin, write stdout to siesta.log for parsing
-#tee input.fdf | docker run --rm -i -v $(pwd):$HOME siesta 2>&1 | tee siesta.log > "$BBM_JOB_DIR"/siesta.log
-# siesta cmds in docker can be overridden
-tee input.fdf | docker run --rm -i -v $(pwd):$HOME siesta bash -l -c "mpirun -np 16 siesta" 2>&1 | tee siesta.log > "$BBM_JOB_DIR"/siesta.log
-
+tee input.fdf | mpirun -np 12 2>&1 | tee siesta.log > "$BBM_JOB_DIR"/siesta.log
 
 # 3. save important files to bbm job starting dir
 # require gosh >= 0.0.25
