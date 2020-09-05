fa2194d8b5378997057dc925933af49a4361c0ca
Wenping Guo <ybyygu@gmail.com>
2020-09-05 10:45:32 +0800

    VASP: 添加固定坐标优化模板(selective dynamic)

--- vasp/opt_selective_dynamics/submit.sh
+++ vasp/opt_selective_dynamics/submit.sh
@@ -3,7 +3,7 @@
 # load VASP related environment variables
 source /share/apps/vasp/env.rc
 
-## Create POSCAR from stdin with user defined template
+## Create POSCAR from stdin with user defined template (input.tera)
 cat > POSCAR
 
 ## Prepare other input files
@@ -14,15 +14,15 @@ cp "$BBM_JOB_DIR/POTCAR" .
 cp "$BBM_JOB_DIR/KPOINTS" .
 
 # Make VASP resume from last job. This could reduce electronic step costs.
-cp "$BBM_JOB_DIR/WAVECAR" . 2>/dev/null
-cp "$BBM_JOB_DIR/CHGCAR" . 2>/dev/null
+# cp "$BBM_JOB_DIR/WAVECAR" . 2>/dev/null
+# cp "$BBM_JOB_DIR/CHGCAR" . 2>/dev/null
 
 # PLEASE CHANGE
 # submit vasp.
-mpirun /share/apps/vasp/bin/vasp > /dev/null
+mpirun /share/apps/vasp/bin/vasp > "$BBM_JOB_DIR"/vasp.log
 
 # save CHGCAR and WAVECAR files for next step
-cp CHGCAR WAVECAR "$BBM_JOB_DIR/" 2>/dev/null
+# cp CHGCAR WAVECAR "$BBM_JOB_DIR/" 2>/dev/null
 
 # PLEASE CHANGE
 # extract energy and forces from OUTCAR
