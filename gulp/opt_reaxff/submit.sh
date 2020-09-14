c291a97ae815eb983eeda7b82596e98b87cce384
Wenping Guo <ybyygu@gmail.com>
2020-09-14 13:09:36 +0800

    更新env.rc, 改进可移植性

--- gulp/opt_reaxff/submit.sh
+++ gulp/opt_reaxff/submit.sh
@@ -4,7 +4,7 @@ source /share/apps/gulp/env.rc
 # read molecular stream from stdin, redirect it to geom_end.gen
 echo @model_properties_format_version 0.1
 echo @energy
-gulp 2> err.log | tee gulp.log | grep "Final energy ="|awk '{print $4}'
+tee gulp.gin | mpirun gulp52.manylinux-gfortran-openmpi4 2> err.log | tee gulp.log | grep "Final energy ="|awk '{print $4}'
 echo @structure
 # skip first two lines
 tail -n +3 final.xyz
